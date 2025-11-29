3c8f86dc44e741d9453e4d7e0fbb2d432f7a1b18
alexchiu <tainancgw@gmail.com>
2025-11-29 18:07:17 +0800

    add-cfgs-for-pp-eslint

---
+++ .eslintrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/recommended', '@vue/airbnb'],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+
+    // recommended by Vetur
+    // 'vue/html-self-closing': 'on',
+
+    // Disable max-len
+    'max-len': 'off',
+
+    // add parens ony when required in arrow function
+    'arrow-parens': ['error', 'as-needed'],
+
+    // add new line above comment
+    'lines-around-comment': [
+      'error',
+      {
+        beforeBlockComment: true,
+        beforeLineComment: true,
+        allowBlockStart: true,
+        allowClassStart: true,
+        allowObjectStart: true,
+        allowArrayStart: true,
+      },
+    ],
+
+    'linebreak-style': 'off',
+
+    // add new line above comment
+    'newline-before-return': 'error',
+
+    // add new line below import
+    'import/newline-after-import': ['error', { count: 1 }],
+
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+
+    'global-require': 'off',
+    'no-underscore-dangle': 'off',
+    camelcase: 'off',
+    'no-param-reassign': 'off',
+  },
+};
